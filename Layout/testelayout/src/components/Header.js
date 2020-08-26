a7b8b023558173a89267e12dc72a93dead858a2a
andersonr <anderson.rissardi@hotmail.com>
2020-08-25 21:45:13 -0300

    Criado aplicação React para começar a aplicar o estilos do Material-ui

---
+++ Layout/testelayout/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+
+// eslint-disable-next-line no-unused-vars
+class Header extends Component {
+  render() {
+    return (<header>
+                <Button variant="outlined" color="secondary">
+                    Teste
+                </Button>
+          </header>);
+  }
+}
+
+export default Header;
