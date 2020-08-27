4d8ec0d8c20d5f85aedf93de0596d0c9209b0b06
Ricardo Colzani <6742811+rcolzani@users.noreply.github.com>
2020-08-27 20:31:41 -0300

    First server commit. Create user and sessions
    Create user and session

---
+++ server/src/routes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
+
+// import multer from 'multer';
+// import multerConfig from './config/multer';
+
+const routes = new Router();
+// const upload = multer(multerConfig);
+// para upload de arquivos.
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+// routes.post('/files', upload.single('file'), FileController.store);
+// para upload de arquivos.
+
+export default routes;
