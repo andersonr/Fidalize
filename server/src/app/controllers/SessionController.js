2a3bd6501b8b18ecec2bb7c92c8efb2d881abe32
andersonr <anderson.rissardi@hotmail.com>
2020-10-21 12:47:08 -0300

    Alteradas mensagens de erro

--- server/src/app/controllers/SessionController.js
+++ server/src/app/controllers/SessionController.js
@@ -13,7 +13,9 @@ class SessionController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
+      return res
+        .status(400)
+        .json({ error: 'Erro ao validar dados de entrada!!' });
     }
 
     const { email, password } = req.body;
@@ -21,11 +23,11 @@ class SessionController {
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(401).json({ error: 'User not found' });
+      return res.status(401).json({ error: 'Usuário não encontrado!' });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not match' });
+      return res.status(401).json({ error: 'Senha incorreta!' });
     }
 
     const { id, name, provider } = user;
--- server/src/app/controllers/UserController.js
+++ server/src/app/controllers/UserController.js
@@ -15,13 +15,15 @@ class UserController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
+      return res
+        .status(400)
+        .json({ error: 'Dados incorretos! Verifique os dados inseridos.' });
     }
 
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res.status(400).json({ error: 'user already exists' });
+      return res.status(400).json({ error: 'Este usuário já está em uso!' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
@@ -50,7 +52,9 @@ class UserController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
+      return res
+        .status(400)
+        .json({ error: 'Dados incorretos! Verifique os dados inseridos.' });
     }
 
     const { email, oldPassword } = req.body;
@@ -63,12 +67,12 @@ class UserController {
       });
 
       if (userExists) {
-        return res.status(400).json({ error: 'user already exists' });
+        return res.status(400).json({ error: 'Este usuário já está em uso!' });
       }
     }
 
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
-      return res.status(401).json({ error: 'Old password does not match' });
+      return res.status(401).json({ error: 'Senha antiga está incorreta!' });
     }
 
     const { id, name, provider } = await user.update(req.body);
